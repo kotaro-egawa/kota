d0520c6b8f350a515dd551248618f587e459dcd5
kotaro-egawa <kota19981015@gmail.com>
2020-04-19 16:11:51 +0900

    台湾の追加とその他編集

---
+++ taiwan/taiwan.js
@@ -0,0 +1,14 @@
+/* サイドバー */
+
+$(function() {
+
+    $(".nav-item").mouseover(function() {
+
+        $(this).css("font-size", "30px");
+    });
+
+    $(".nav-item").mouseout(function() {
+
+        $(this).css("font-size", "20px")
+    })
+});
